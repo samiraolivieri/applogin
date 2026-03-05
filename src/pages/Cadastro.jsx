670c34e8fbd8ea3c2f7cc5f43b0f9f60ea8a4ccf
Samira Olivieri <art0066441@aluno.firjansenaisesi.com.br>
2026-03-05 16:03:13 -0300

    quarto commit

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,10 +1,50 @@
-function Cadastro() {
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-    return (
-     <>
-     <h1>Cadastro</h1>
-     </>
-    )
-  }
-  
-  export default Cadastro
+function Cadastro() {
+const[login, setLogin] = useState("")
+const[senha, setSenha] = useState("")
+const navigate = useNavigate()
+
+async function cadastrar(e){
+  e.preventDefault()
+  await fetch("http://localhost:3020/usuario",{
+    method: "POST",
+    headers:{
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      login,
+      senha
+    })
+  })
+  alert("Usuário cadastrado!!")
+  navigate("/")
+}
+
+  return (
+    <div>
+      <h2>Novo Cadastro</h2>
+
+      <form onSubmit={cadastrar}>
+        <input
+          type="text"
+          placeholder="Login"
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+        /> <br /><br />
+
+        <input
+          type="password"
+          placeholder="Senha"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+        /> <br /><br />
+
+        <button type="submit">Cadastrar</button>
+      </form>
+    </div>
+  );
+}
+
+export default Cadastro;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,57 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 
 
 function Login() {
+const [login, setLogin] = useState("")
+const [senha, setSenha] = useState("")
+const [erro, setErro] = useState("")
+const navigate  = useNavigate()
+
+
+async function validarLogin(e){
+
+  e.preventDefault()
+  const response = await fetch("http://localhost:3020/usuario")
+  const usuarios = await response.json()
+  const usuarioValido = usuarios.find(
+  (u) => u.login === login && u.senha === senha
+  )
+
+  if(usuarioValido){
+    navigate("/home")
+  }
+    else{
+      setErro("Login ou Senha errados!")
+    }
+  }
 
     return (
      <>
-     <h1>Login</h1>
-     <Link to={"/cadastro"} >Cadastro</Link>
+    <h1>Login</h1>
+    <form onSubmit={validarLogin}>
+        <input
+          type="text" 
+          placeholder="Login"
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+        /> 
+
+        <br />
+        <br />
+
+         <input
+          type="password" 
+          placeholder="Senha"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+        />
+        <button type="submit">Entrar</button>
+     
+    </form>
+    {erro && <p style={{color: "red"}}>{erro}</p>}
+     <Link to={"/cadastro"} >Cadastro</Link> <br/>
+     
      </>
     )
   }
